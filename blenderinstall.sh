38ef454831821b9d3da1f0409385e7bd140db1cf
Niklasw99 <32064953+Niklasw99@users.noreply.github.com>
2023-05-16 03:32:37 +0200

    Update blenderinstall.sh

--- blenderinstall.sh
+++ blenderinstall.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir /home/blender
 cd /home/blender
-sudo chmod 775 /home/blender
+sudo chmod 777 /home/blender
 curl -L -O "https://mirrors.dotsrc.org/blender/release/Blender3.5/blender-3.5.1-linux-x64.tar.xz"
 tar -xf blender-3.5.1-linux-x64.tar.xz
 sudo pacman --noconfirm -R blender
